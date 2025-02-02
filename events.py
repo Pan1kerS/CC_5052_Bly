f6ac84c59bdef71819614bbed8ca5547570f349f
Svyatoslav Panikarovskiy <pan1ker03@bk.ru>
2025-02-02 16:24:28 +0600

    Перепись кода

--- events.py
+++ events.py
@@ -1,14 +1,13 @@
 import disnake
 from disnake.ext import commands
-from config import WELCOME_CHANNEL_ID, DEFAULT_ROLES_IDS, CENSORED_WORDS
+from config import DEFAULT_ROLES_IDS, WELCOME_CHANNEL_ID
 
-async def setup_events(bot: commands.Bot):
+async def events_bot(bot: commands.Bot):
     # Оповещение, что бот запущен
     @bot.event
     async def on_ready():
-        print(f"СС 5052 Bly прибыл в штаб")
+        print(f'СС 5052 Bly прибыл в расположение штаба')
 
-    # Новприбывшие бойцы + авто-выдача ролей
     @bot.event
     async def on_member_join(member):
         try:
@@ -18,27 +17,12 @@ async def setup_events(bot: commands.Bot):
             channel = member.guild.get_channel(WELCOME_CHANNEL_ID)
 
             embed = disnake.Embed(
-                title="Новоприбывший боец!",
-                description=f"{member.name}#{member.discriminator}",
-                color=0xbda600
+                title = "Новоприбовший боец!",
+                description = f"{member.name}#{member.discriminator}",
+                color = 0xbda600
             )
 
             await channel.send(embed=embed)
 
         except Exception as e:
             print(f"Error in on_member_join: {e}")
-
-    # Провека сообщений
-    @bot.event
-    async def on_message(message):
-        await bot.process_commands(message)
-
-        if message.author.bot:
-            return
-
-        content = message.content.lower()
-
-        if any(censored_word in content for censored_word in CENSORED_WORDS):
-            await message.delete()
-            await message.channel.send(f"{message.author.mention} Отставить мандалорскую лексику!")
-            return
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import disnake
+from config import  BOT_TOKEN, TEST_GUILDS
 from disnake.ext import commands
-from config import BOT_TOKEN, TEST_GUILDS
-from events import setup_events
-import os
+from events import events_bot
+import asyncio
 
-# Инициализация бота
 bot = commands.Bot(
     command_prefix="/",
     help_command=None,
@@ -12,24 +11,9 @@ bot = commands.Bot(
     test_guilds=TEST_GUILDS
 )
 
-
-async def load_cogs(bot):
-    for folder in os.listdir('./commands'):
-        if os.path.isdir(f'./commands/{folder}'):
-            for filename in os.listdir(f'./commands/{folder}'):
-                if filename.endswith('.py'):
-                    cog_name = filename[:-3]
-                    await bot.load_extension(f'commands.{folder}.{cog_name}')
-
 async def main():
-    # Регистрация событий
-    await setup_events(bot)
-    # Регистрация команд
-    await load_cogs(bot)
-    # Запуск бота
-    bot.run(BOT_TOKEN)
+    await events_bot(bot)
+    await bot.start(BOT_TOKEN)
 
 if __name__ == "__main__":
-    import asyncio
-
     asyncio.run(main())
