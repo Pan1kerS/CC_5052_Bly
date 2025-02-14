b76877d4e2b0e11d46a5ca788c54e4dbdeb5ace9
Svyatoslav Panikarovskiy <pan1ker03@bk.ru>
2025-02-14 17:37:52 +0600

    Add files via upload
    MEGA UPDATE

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
 import disnake
-from config import BOT_TOKEN, TEST_GUILDS, DEFAULT_ROLES_IDS, WELCOME_CHANNEL_ID, CENSORED_WORDS
 from disnake.ext import commands
-import os
-
-intents=disnake.Intents.all()
-bot = commands.Bot(
-    command_prefix="/",
-    help_command=None,
-    intents=intents,
-    test_guilds=TEST_GUILDS
+from config import (
+    BOT_TOKEN, TEST_GUILDS, CHANNELS, ROLES, 
+    CENSORED_WORDS
 )
+import asyncio
+import os
+import traceback
 
-async def load_extensions():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")
+intents = disnake.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents, test_guilds=TEST_GUILDS)
 
 @bot.event
 async def on_ready():
     print(f'СС 5052 Bly прибыл в расположение штаба')
-    await load_extensions()
 
 @bot.event
 async def on_member_join(member):
     try:
-        roles = [member.guild.get_role(role_id) for role_id in DEFAULT_ROLES_IDS if member.guild.get_role(role_id)]
+        # Добавляем роли по умолчанию
+        roles = [member.guild.get_role(role_id) for role_id in ROLES['DEFAULT'] if member.guild.get_role(role_id)]
         await member.add_roles(*roles)
 
-        channel = member.guild.get_channel(WELCOME_CHANNEL_ID)
-
+        # Отправляем сообщение в канал приветствия
+        channel = member.guild.get_channel(CHANNELS['WELCOME'])
         embed = disnake.Embed(
-            title = "Новоприбовший боец!",
-            description = f"{member.name}#{member.discriminator}",
-            color = 0xbda600
+            title="Новоприбовший боец!",
+            description=f"{member.name}#{member.discriminator}",
+            color=0xbda600
         )
-
         await channel.send(embed=embed)
 
     except Exception as e:
@@ -48,10 +43,36 @@ async def on_message(message):
         return
 
     content = message.content.lower()
-
-    if any(censored_word in content for censored_word in CENSORED_WORDS):
+    if any(word in content for word in CENSORED_WORDS):
         await message.delete()
         await message.channel.send(f"{message.author.mention} Отставить мандалорскую лексику!")
         return
+
+INITIAL_EXTENSIONS = [
+    'cogs.report_op',
+    'cogs.report_training',
+    'cogs.report_holiday',
+    'cogs.report_up_down',
+    'cogs.report_attestation',
+    'cogs.report_recommendation',
+    'cogs.report_reprimand',
+    'cogs.setup_buttons',
+    'cogs.clear',
+    'cogs.me',
+    'cogs.tables'
+]
+
+def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            try:
+                bot.load_extension(f"cogs.{filename[:-3]}")
+                print(f"Загружен ког {filename}")
+            except Exception as e:
+                print(f"Ошибка загрузки кога {filename}: {str(e)}")
+                traceback.print_exc()
+                print(f"Полная ошибка: {repr(e)}")
+
+load()
 
 bot.run(BOT_TOKEN)
